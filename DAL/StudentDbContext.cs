d7432768fafb15511d2775b25a560531dbda1d54
Daniel <daniel.gomez@studentpartner.com>
2020-01-08 17:19:53 -0500

    DotVVM

---
+++ DAL/StudentDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DotVVMcrud.DAL.Entities;
+namespace DotVVMcrud.DAL
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
